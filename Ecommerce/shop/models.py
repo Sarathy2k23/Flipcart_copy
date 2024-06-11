26a2e64c9947be70895233854f1b793cb2dc25c6
Sarathy2K23 <parthasarathy172001@gmail.com>
2024-06-12 02:48:15 +0530

    First Flipcart commit

---
+++ Ecommerce/shop/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Users(models.Model):
+    Email=models.EmailField(max_length=100)
+    Password=models.models.CharField(max_length=50)
+class Person(models.Model):
+    First_name=models.CharField(max_length=50)
+    Last_name=models.CharField(max_length=50)    
